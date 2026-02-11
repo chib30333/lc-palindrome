80d79ceb8109819495d5e8550b4b911cbade8784
chib30333 <chib30333@gmail.com>
2026-02-11 08:23:24 +0100

    feat: implement palindrome checking logic

--- main.cpp
+++ main.cpp
@@ -1,1 +1,31 @@
+#include <vector>
 #include <iostream>
+#include <cmath>
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0) return false;
+        else if (x == 0) return true;
+        
+        std::vector<int> array;
+        int deep = std::floor(std::log10(x)) + 1;
+        int rotate = 0;
+
+        while(true) {
+            if(rotate >= deep / 2) break;
+
+            int right = x % 10;
+            int left = x / std::floor(std::pow(10, deep - 1));
+
+            if(right != left) return false;
+
+            x = (x - left * std::floor(std::pow(10, deep - 1))) / 10;
+            deep -= 2;
+
+            rotate++;
+        }
+
+        return true;
+    }
+};
