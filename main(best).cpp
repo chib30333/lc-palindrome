e2a9e5bc63a42eaf248a324dbedd37919084cbc0
chib30333 <chib30333@gmail.com>
2026-02-11 09:25:17 +0100

    feat: finding best solutions

---
+++ main(best).cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x < 0) return false;
+        if(x % 10 == 0 && x != 0) return false;
+
+        int reverse = 0;
+
+        while(x > reverse) {
+            reverse = reverse * 10 + (x % 10);
+            x = x / 10;
+        }
+
+        return x == reverse || x == reverse / 10;
+    }
+};
